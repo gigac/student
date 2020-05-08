0cdec5225125ef941a3171839f3e78475e530f41
Argir Popov <argirco.popov@gmail.com>
2020-05-08 22:02:19 +0200

    Introduce not found exception

---
+++ src/Domain/Shared/NotFoundException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Domain\Shared;
+
+/**
+ * Class NotFoundException
+ *
+ * @package App\Domain\Shared
+ */
+class NotFoundException extends \Exception
+{
+
+}
---
+++ src/Domain/Student/StudentNotFoundException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Domain\Student;
+
+use App\Domain\Shared\NotFoundException;
+
+/**
+ * Class StudentNotFoundException
+ *
+ * @package App\Domain\Student
+ */
+class StudentNotFoundException extends NotFoundException
+{
+
+}
